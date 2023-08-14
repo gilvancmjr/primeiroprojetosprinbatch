27839c51e4777316a60c7003150de829bfb077b4
Gilvan Júnior <98542272+gilvancmjr@users.noreply.github.com>
2023-08-14 10:24:53 -0300

    update

---
+++ src/main/java/com/udemy/primeiroprojetosprinbatch/config/PropsConfig.java
@@ -0,0 +1,19 @@
+package com.udemy.primeiroprojetosprinbatch.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.core.io.FileSystemResource;
+
+@Configuration
+public class PropsConfig {
+	
+	@Bean
+	public PropertySourcesPlaceholderConfigurer config() {
+		PropertySourcesPlaceholderConfigurer configurer = new PropertySourcesPlaceholderConfigurer();
+		configurer.setLocation(
+				new FileSystemResource("/Users/gilvanCarvalho/Documents/GitHub/primeiroprojetosprinbatch/application.properties"));
+		return configurer;
+	}
+
+}
